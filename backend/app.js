12723c8987680637d399d0c8b57a041459a23500
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2024-12-17 18:38:16 +0530

    user registration created

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
-import express from "express"
+import express, { urlencoded } from "express"
 const app = express.Router()
+import userRoutes from './routes/user.routes.js'
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use('/users', userRoutes)
 
 app.get('/', (req, res) => {
     res.send("hello world this")
